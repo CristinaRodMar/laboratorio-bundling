5affcc6816e2ee5215b39cf70f0340fd6c50f324
Nombre prueba <prueba@ejemplo.com>
2025-12-03 12:02:02 +0100

    Completo el apartado opcional de Vite

--- Vite/vite.config.ts
+++ Vite/vite.config.ts
@@ -3,12 +3,29 @@ import checker from "vite-plugin-checker";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { analyzer } from "vite-bundle-analyzer";
+import { compression } from "vite-plugin-compression2"; // ← nuevo
 
 export default defineConfig({
     plugins: [
-        checker({ typescript: true }),
-        tailwindcss(),
-        react(),
-        analyzer({ analyzerMode: "static", openAnalyzer: true }),
+    checker({ typescript: true }),
+    tailwindcss(),
+    react(),
+    analyzer({ analyzerMode: "static", openAnalyzer: true }),
+    compression({
+    algorithms: ["gzip", "brotliCompress"], 
+    exclude: [/\.(br)$/, /\.(gz)$/],       
+    deleteOriginalAssets: false,           
+    threshold: 1024,                     
+    }),
     ],
+
+    build: {
+        rollupOptions: {
+        output: {
+            chunkFileNames: "assets/[name]-[hash].js",
+            assetFileNames: "assets/[name]-[hash][extname]",
+            entryFileNames: "assets/[name]-[hash].js",
+        },
+        },
+    },
 });
